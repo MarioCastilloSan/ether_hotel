8b0739996fac83cbf273c2e4cd53136c5213df1f
Mario <mariocastillosan@gmail.com>
2024-07-10 16:20:21 -0400

    update: added dates, and discount codes

--- contracts/hotel.sol
+++ contracts/hotel.sol
@@ -3,48 +3,87 @@ pragma solidity ^0.8.13;
 
 contract HotelBooking {
     address payable public owner;
-    uint private room_price = 0.01 ether;
-    mapping(uint => bool) public booked_dates;
-    mapping(uint => address) public booker;
-
-
-    constructor() {
-        owner = payable(msg.sender); 
+    
+    enum RoomType { SuitePresidencial, HabitacionEjecutiva, SuiteJardin, HabitacionDobleDeluxe, HabitacionRomantica, SuitePanoramica, HabitacionEstandar, HabitacionLoft, SuiteBienestar }
+    
+    struct Room {
+        uint basePrice;
+        bool isBooked;
+        address booker;
+        uint startDate;
+        uint endDate;
     }
-
+    
+    mapping(RoomType => mapping(uint => Room)) public rooms;
+    
+    constructor() {
+        owner = payable(msg.sender);
+        // Inicializar precios base
+        rooms[RoomType.SuitePresidencial][0].basePrice = 0.05 ether;
+        rooms[RoomType.HabitacionEjecutiva][0].basePrice = 0.04 ether;
+        rooms[RoomType.SuiteJardin][0].basePrice = 0.035 ether;
+        rooms[RoomType.HabitacionDobleDeluxe][0].basePrice = 0.03 ether;
+        rooms[RoomType.HabitacionRomantica][0].basePrice = 0.025 ether;
+        rooms[RoomType.SuitePanoramica][0].basePrice = 0.045 ether;
+        rooms[RoomType.HabitacionEstandar][0].basePrice = 0.02 ether;
+        rooms[RoomType.HabitacionLoft][0].basePrice = 0.015 ether;
+        rooms[RoomType.SuiteBienestar][0].basePrice = 0.05 ether;
+    }
+    
     modifier onlyOwner() {
         require(msg.sender == owner, "Only the owner can perform this action");
         _;
     }
-
-    function get_room_price() public view returns(uint){
-        return room_price;
+    
+    function setRoomPrice(RoomType roomType, uint _newPrice) public onlyOwner {
+        rooms[roomType][0].basePrice = _newPrice;
+    }
+    
+    function calculatePrice(RoomType roomType, uint numDays, bool applyDiscount) public view returns (uint) {
+        uint price = rooms[roomType][0].basePrice + (numDays * 0.01 ether);
+        if (applyDiscount) {
+            return price / 2;
+        }
+        return price;
     }
 
-    function setroom_price(uint _newPrice) public onlyOwner {
-        room_price = _newPrice;
-    }
+    function bookRoom(RoomType roomType, uint startDate, uint endDate, bool applyDiscount) public payable {
+        uint numDays = (endDate - startDate) / 1 days + 1;
+        uint totalPrice = calculatePrice(roomType, numDays, applyDiscount);
+        require(msg.value == totalPrice, "Incorrect amount of ETH sent");
 
-   function bookRoom(uint _date) public payable {
-    require(msg.value == room_price, "Please submit the correct amount of ETH");
-    require(!booked_dates[_date], "This date is already booked");
-    booked_dates[_date] = true;
-    booker[_date] = msg.sender; 
+        for (uint date = startDate; date <= endDate; date += 1 days) {
+            require(!rooms[roomType][date].isBooked, "One or more dates are already booked");
         }
 
-
-    function checkAvailability(uint _date) public view returns(bool) {
-        return !booked_dates[_date];
-    }
-
-    function cancelBooking(uint _date) public {
-    require(msg.sender == booker[_date], "Only the booker can cancel this reservation");
-    require(booked_dates[_date], "This date is not booked");
-    booked_dates[_date] = false;
-    booker[_date] = address(0); 
+        for (uint date = startDate; date <= endDate; date += 1 days) {
+            rooms[roomType][date].isBooked = true;
+            rooms[roomType][date].booker = msg.sender;
+            rooms[roomType][date].startDate = startDate;
+            rooms[roomType][date].endDate = endDate;
         }
-
-
+    }
+    
+    function checkAvailability(RoomType roomType, uint startDate, uint endDate) public view returns (bool) {
+        for (uint date = startDate; date <= endDate; date += 1 days) {
+            if (rooms[roomType][date].isBooked) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    function cancelBooking(RoomType roomType, uint startDate, uint endDate) public {
+        for (uint date = startDate; date <= endDate; date += 1 days) {
+            require(msg.sender == rooms[roomType][date].booker, "Only the booker can cancel this reservation");
+            require(rooms[roomType][date].isBooked, "This date is not booked");
+            rooms[roomType][date].isBooked = false;
+            rooms[roomType][date].booker = address(0);
+            rooms[roomType][date].startDate = 0;
+            rooms[roomType][date].endDate = 0;
+        }
+    }
+    
     function withdrawFunds() public onlyOwner {
         owner.transfer(address(this).balance);
     }
--- src/pages/ProjectDisplay.js
+++ src/pages/ProjectDisplay.js
@@ -2,41 +2,86 @@ import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { ProjectList } from '../helpers/ProjectList';
 import getContractInstance from '../blockchain/web3';
-import Button from '@mui/material/Button'; 
-import '../styles/ProjectDisplay.css'
+import Web3 from 'web3';
+import Button from '@mui/material/Button';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+import { differenceInCalendarDays } from 'date-fns';
+import '../styles/ProjectDisplay.css';
 
 function ProjectDisplay() {
     const { id } = useParams();
     const project = ProjectList[id];
     const [isBooked, setIsBooked] = useState(false);
     const [isBooker, setIsBooker] = useState(false);
+    const [startDate, setStartDate] = useState(null);
+    const [endDate, setEndDate] = useState(null);
+    const [useDiscount, setUseDiscount] = useState(false);
+    const [discountCode, setDiscountCode] = useState("");
+    const [isDiscountValid, setIsDiscountValid] = useState(false);
+    const [price, setPrice] = useState(null);
+
+    const roomType = id; // Assuming roomType is the same as id for simplicity
 
     useEffect(() => {
-        const checkAvailability = async () => {
-            const contract = await getContractInstance();
-            const available = await contract.methods.checkAvailability(id).call();
-            setIsBooked(!available);
-
-            if (!available) {
-                const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-                const booker = await contract.methods.booker(id).call();
-                setIsBooker(accounts[0].toLowerCase() === booker.toLowerCase());
+        const fetchPrice = async () => {
+            try {
+                const contract = await getContractInstance();
+                const basePrice = await contract.methods.calculatePrice(roomType, 1, false).call();
+                setPrice(Web3.utils.fromWei(basePrice, 'ether'));
+            } catch (error) {
+                console.error("Error fetching price:", error);
             }
         };
 
+        fetchPrice();
+    }, [roomType]);
+
+    useEffect(() => {
+        const checkAvailability = async () => {
+            if (!startDate || !endDate) return;
+            const contract = await getContractInstance();
+            const startDateTimestamp = Math.floor(startDate.getTime() / 1000);
+            const endDateTimestamp = Math.floor(endDate.getTime() / 1000);
+            const available = await contract.methods.checkAvailability(roomType, startDateTimestamp, endDateTimestamp).call();
+            setIsBooked(!available);
+        };
+
         checkAvailability();
-    }, [id]);
+    }, [id, startDate, endDate]);
+
+    const validateDiscountCode = (code) => {
+        // Aquí puedes implementar la lógica para verificar el código de descuento
+        // En este ejemplo simple, asumimos que "DISCOUNT50" es el código válido
+        return code === "profepongameun7";
+    };
 
     const handleReserve = async () => {
         try {
+            if (!startDate || !endDate) {
+                alert('Por favor selecciona las fechas de inicio y fin.');
+                return;
+            }
+
             const contract = await getContractInstance();
             const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            const price = await contract.methods.get_room_price().call(); 
+            const numDays = differenceInCalendarDays(endDate, startDate) + 1;
+            const startDateTimestamp = Math.floor(startDate.getTime() / 1000);
+            const endDateTimestamp = Math.floor(endDate.getTime() / 1000);
 
-            await contract.methods.bookRoom(id).send({ from: accounts[0], value: price });
+            // Verificar si la habitación ya está reservada en el registro final
+            const isAvailable = await contract.methods.checkAvailability(roomType, startDateTimestamp, endDateTimestamp).call();
+            if (!isAvailable) {
+                alert('La habitación ya está reservada para las fechas seleccionadas.');
+                return;
+            }
+
+            // Calcular el precio total con o sin descuento
+            const totalPriceWei = await contract.methods.calculatePrice(roomType, numDays, isDiscountValid).call();
+            await contract.methods.bookRoom(roomType, startDateTimestamp, endDateTimestamp, isDiscountValid).send({ from: accounts[0], value: totalPriceWei });
+            alert('Reserva realizada con éxito!');
             setIsBooked(true);
             setIsBooker(true);
-            alert('Reserva realizada con éxito!');
         } catch (error) {
             console.error('Error al hacer la reserva:', error);
             alert('Error al realizar la reserva. Ver consola para más detalles.');
@@ -45,9 +90,12 @@ function ProjectDisplay() {
 
     const handleCancelReservation = async () => {
         try {
+            if (!startDate || !endDate) return;
             const contract = await getContractInstance();
             const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-            await contract.methods.cancelBooking(id).send({ from: accounts[0] });
+            const startDateTimestamp = Math.floor(startDate.getTime() / 1000);
+            const endDateTimestamp = Math.floor(endDate.getTime() / 1000);
+            await contract.methods.cancelBooking(roomType, startDateTimestamp, endDateTimestamp).send({ from: accounts[0] });
             setIsBooked(false);
             setIsBooker(false);
             alert('Reserva cancelada con éxito!');
@@ -57,19 +105,57 @@ function ProjectDisplay() {
         }
     };
 
+    const handleDiscountCodeChange = (event) => {
+        const code = event.target.value;
+        setDiscountCode(code);
+        setIsDiscountValid(validateDiscountCode(code));
+    };
+
     return (
         <div className='project'>
             <h1>{project.name}</h1>
             <img src={project.image} alt={project.name}/>
             <p><b>{project.info}</b></p>
+            {price && <p><b>Precio por noche: {price} ETH</b></p>}
+            <div>
+                <DatePicker
+                    selected={startDate}
+                    onChange={(date) => setStartDate(date)}
+                    selectsStart
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={new Date()}
+                    placeholderText="Fecha de inicio"
+                />
+                <DatePicker
+                    selected={endDate}
+                    onChange={(date) => setEndDate(date)}
+                    selectsEnd
+                    startDate={startDate}
+                    endDate={endDate}
+                    minDate={startDate || new Date()}
+                    placeholderText="Fecha de fin"
+                />
+            </div>
             <Button variant="contained" color="primary" onClick={handleReserve} disabled={isBooked}>
-            {isBooked ? "Reservado" : "Reservar"}
+                {isBooked ? "Reservado" : "Reservar"}
             </Button>
             {isBooker && (
                 <Button variant="outlined" color="secondary" onClick={handleCancelReservation}>
                     Cancelar Reserva
                 </Button>
             )}
+            <div>
+                <Button variant="contained" color="primary" onClick={() => setUseDiscount(!useDiscount)}>
+                    {useDiscount ? "Cancelar Descuento" : "Usar Código de Descuento"}
+                </Button>
+                {useDiscount && (
+                    <div>
+                        <input type="text" value={discountCode} onChange={handleDiscountCodeChange} placeholder="Código de Descuento" />
+                        {isDiscountValid ? <p>Código válido</p> : <p>Código inválido</p>}
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
